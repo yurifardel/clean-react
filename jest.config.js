9d2c13a5a7e8dd6939bdf2936897c5ffd87f1545
yuri fardel <yrnascimento@outlook.com>
2021-09-08 20:05:48 -0300

    fix: problem fix

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.{ts,tsx}',
+    '!<rootDir>/src/main/**/*',
   ],
   coverageDirectory: 'coverage',
   testPathIgnorePatterns: [
