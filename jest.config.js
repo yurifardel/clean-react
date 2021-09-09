0d91f9e38da1587af9632e974eda2bbd5bfc7b89
yuri fardel <yrnascimento@outlook.com>
2021-09-08 23:12:55 -0300

    chore: maintenance

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,6 @@ module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.{ts,tsx}',
-    '!<rootDir>/src/main/**/*',
   ],
   coverageDirectory: 'coverage',
   testPathIgnorePatterns: [
