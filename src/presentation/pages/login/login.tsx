0847261f4ec521e37d0086fe65f3392a94671aa6
yuri fardel <yrnascimento@outlook.com>
2021-10-01 16:55:53 -0300

    refactor: move header to a separate file

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,16 +1,13 @@
 import React from 'react'
 import Spinner from '@/presentation/components/spinner/spinner'
-import Logo from '@/presentation/components/logo/logo'
+import LoginHeader from '@/presentation/components/login-header/login-header'
 
 import Styles from './login-styles.scss'
 
 const Login: React.FC = () => {
   return (
     <div className={Styles.login}>
-      <header className={Styles.header}>
-        < Logo/>
-        <h1>$Dev</h1>
-      </header>
+      <LoginHeader />
       <form className={Styles.form} action="">
         <h2>Login</h2>
         <div className={Styles.inputWrap}>
