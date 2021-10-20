91397b888605deca5d3a2d5e67da31b6ca6e72bf
yuri fardel <yrnascimento@outlook.com>
2021-10-20 12:27:45 -0300

    refactor: move authenticationSpy to its own file

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -5,7 +5,7 @@ import Context from '@/presentation/contexts/form/form-context'
 type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
 const Input: React.FC<Props> = (props: Props) => {
-  const { state, setState, validation } = useContext(Context)
+  const { state, setState } = useContext(Context)
   const error = state[`${props.name}Error`]
 
   const enableInput = (event: React.FocusEvent<HTMLInputElement>): void => {
--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,21 +1,9 @@
 import React from 'react'
 import { cleanup, render, RenderResult, fireEvent } from '@testing-library/react'
 import Login from '@/presentation/pages/login/login'
-import { ValidationStub } from '@/presentation/test'
+import { ValidationStub, AuthenticationSpy } from '@/presentation/test'
+
 import faker from 'faker'
-import { Authentication, AuthenticationParams } from '@/domain/usecases'
-import { AccountModel } from '@/domain/models'
-import { mockAccountModel } from '@/domain/test'
-
-class AuthenticationSpy implements Authentication {
-  account = mockAccountModel()
-  params: AuthenticationParams
-
-  async auth (params: AuthenticationParams): Promise<AccountModel> {
-    this.params = params
-    return Promise.resolve(this.account)
-  }
-}
 
 type SutTypes = {
   sut: RenderResult
