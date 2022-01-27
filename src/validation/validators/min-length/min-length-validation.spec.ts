64d7ab2b83c620a1e1660a6b9100e00e5e890e9c
yuri fardel <yrnascimento@outlook.com>
2022-01-26 23:11:52 -0300

    fix: fixed min-length

--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -21,9 +21,8 @@ describe('MinLengthValidation', () => {
   })
 
   test('should return falsy if field does not exists in schema', () => {
-    const field = faker.database.column()
-    const sut = makeSut(field)
-    const error = sut.validate({ [field]: faker.random.alphaNumeric(5) })
+    const sut = makeSut(faker.database.column())
+    const error = sut.validate({ [faker.database.column()]: faker.random.alphaNumeric(5) })
     expect(error).toBeFalsy()
   })
 })
